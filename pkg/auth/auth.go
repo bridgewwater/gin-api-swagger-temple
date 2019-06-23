e1f9219b603d822d5f9c3557859daf355a263845
sinlov <sinlovppt@gmail.com>
2019-06-23 15:59:34 +0800

    add auth.go base utils

---
+++ pkg/auth/auth.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"regexp"
+)
+
+var urlCheckList = [...]string{
+	"/v1/board/burden",
+}
+
+var urlMatchList = [...]string{
+	"/v1/board/record/date/.*",
+}
+
+var superAccessToken = "557c0689faf80e61daeee6e343"
+
+func GinUnAuthCheck(c *gin.Context) error {
+	authorization := c.GetHeader("x-access-token")
+	if authorization == "" {
+		return fmt.Errorf("no head x-access-token")
+	}
+	xAccessToken := superAccessToken
+	url := c.Request.URL.String()
+
+	for _, val := range urlCheckList {
+		if url == val {
+			if authorization == xAccessToken {
+				return nil
+			}
+		}
+	}
+
+	for _, matchStr := range urlMatchList {
+		matched, _ := regexp.MatchString(matchStr, url)
+		if matched {
+			if authorization == xAccessToken {
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("other check, please fix Auth check error")
+}
+
+func GinUnAuthHead(c *gin.Context, key, val string) error {
+	xAccessToken := c.GetHeader(key)
+	if xAccessToken == val {
+		return nil
+	}
+	return fmt.Errorf("can not pass auth")
+}
