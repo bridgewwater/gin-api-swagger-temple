cbcde051a2e657b4f6d36af20e84b7831bb71e34
sinlov <sinlovgmppt@gmail.com>
2023-08-16 10:31:12 +0800

    feat: add MockQueryStrFrom kit

--- api/v1/handler/biz_test/z_kit_gin_test.go
+++ api/v1/handler/biz_test/z_kit_gin_test.go
@@ -1,11 +1,14 @@
 package biz_test
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -38,4 +41,61 @@ func FetchGinRunMode() string {
 		ginMode = gin.TestMode
 	}
 	return ginMode
+}
+
+// MockQueryStrFrom
+//
+//	make query string from params
+func MockQueryStrFrom(params interface{}) (result string) {
+	if params == nil {
+		return
+	}
+	value := reflect.ValueOf(params)
+
+	switch value.Kind() {
+	case reflect.Struct:
+		var formName string
+		for i := 0; i < value.NumField(); i++ {
+			if formName = value.Type().Field(i).Tag.Get("form"); formName == "" {
+				// don't tag the form name, use camel name
+				formName = GetCamelNameFrom(value.Type().Field(i).Name)
+			}
+			result += "&" + formName + "=" + fmt.Sprintf("%v", value.Field(i).Interface())
+		}
+	case reflect.Map:
+		for _, key := range value.MapKeys() {
+			result += "&" + fmt.Sprintf("%v", key.Interface()) + "=" + fmt.Sprintf("%v", value.MapIndex(key).Interface())
+		}
+	default:
+		return
+	}
+
+	if result != "" {
+		result = result[1:]
+	}
+	return
+}
+
+// GetCamelNameFrom
+//
+//	get the Camel name of the original name
+func GetCamelNameFrom(name string) string {
+	result := ""
+	i := 0
+	j := 0
+	r := []rune(name)
+	for m, v := range r {
+		// if the char is the capital
+		if v >= 'A' && v < 'a' {
+			// if the prior is the lower-case || if the prior is the capital and the latter is the lower-case
+			if (m != 0 && r[m-1] >= 'a') || ((m != 0 && r[m-1] >= 'A' && r[m-1] < 'a') && (m != len(r)-1 && r[m+1] >= 'a')) {
+				i = j
+				j = m
+				result += name[i:j] + "_"
+			}
+		}
+	}
+
+	result += name[j:]
+	return strings.ToLower(result)
 }
